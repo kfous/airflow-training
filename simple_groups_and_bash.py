58886d357e005adee7e863d92388131cf7a01cfa
kfous <kostasfsks@gmail.com>
2024-05-17 22:10:29 +0300

    New addition of dag

---
+++ simple_groups_and_bash.py
@@ -0,0 +1,21 @@
+import datetime
+
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
+
+
+def simple_function():
+    print("Hello from the task execution")
+
+
+with DAG(
+        dag_id="first_dag_with_python_and_bash",
+        schedule=None,
+):
+    empty_task = EmptyOperator(task_id="empty_task")
+    simple_python_task = PythonOperator(task_id="simple_python_task", python_callable=simple_function)
+    simple_bash_task = BashOperator(task_id="simple_bash_task", bash_command='echo "Hello from bash"')
+
+    [empty_task, simple_python_task] >> simple_bash_task
