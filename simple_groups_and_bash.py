b000297f4e7d3289c849555dbd478a69826b5414
kfous <kostasfsks@gmail.com>
2024-05-17 23:03:09 +0300

    added startdate to second dag

--- simple_groups_and_bash.py
+++ simple_groups_and_bash.py
@@ -9,10 +9,12 @@ from airflow.operators.bash import BashOperator
 def simple_function():
     print("Hello from the task execution")
 
-# Test comment
+# Dag with sequence operators for tasks
 with DAG(
         dag_id="first_dag_with_python_and_bash",
         schedule=None,
+        start_date=datetime(2024, 5, 17),  
+        tags=['fousekis']
 ):
     empty_task = EmptyOperator(task_id="empty_task")
     simple_python_task = PythonOperator(task_id="simple_python_task", python_callable=simple_function)
