d4d34a10c3a1be400e89d711df88252d57eb5d33
kfous <kostasfsks@gmail.com>
2024-05-17 22:56:23 +0300

    added startdate to simpledag

--- simple_dag.py
+++ simple_dag.py
@@ -1,8 +1,11 @@
+from datetime import datetime
 from airflow import DAG
 from airflow.operators.empty import EmptyOperator
 
 with DAG(
-        dag_id="first_dag",
-        schedule=None,
-):
+    dag_id="first_dag",
+    schedule_interval=None,  
+    start_date=datetime(2024, 5, 17),  
+    tags=['fousekis']  
+) as dag:
     empty_task = EmptyOperator(task_id="empty_task")
